4a7f3062c24b26e8cd0c4fe2077d6b7468b28c94
Matsubara-mariko <mariko.m@get-it.ne.jp>
2024-12-10 10:46:42 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,13 @@
+// 変数の宣言
+let height ;
+let weight ;
+
+// 値の代入
+height = 1.7;
+weight = 50;
+
+let bmi = weight / (height * height);
+
+//計算式：[体重(kg)]÷[身長(m)×身長(m)]
+ // コンソールへの出力
+console.log("BMI:", bmi);
